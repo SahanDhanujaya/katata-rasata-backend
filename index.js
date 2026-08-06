2353b0f9d69346b0d888735b0e86b9830dcbecdc
Sahan Dhanujaya <sahandhanujaya18@gmail.com>
2026-08-07 02:49:46 +0530

    update: server cors

--- index.js
+++ index.js
@@ -10,10 +10,26 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-    origin: "https://lakas-take-away.netlify.app/",
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://lakas-take-away.netlify.app/",
+    ],
     credentials: true,
-}));
+    methods: [
+      "GET",
+      "POST",
+      "PUT",
+      "PATCH",
+      "DELETE"
+    ],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization"
+    ]
+  })
+);
 
 app.use(cookieParser());
 
