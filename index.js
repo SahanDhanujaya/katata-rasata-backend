18755ff1c4712a52f8c3433cd4d54447ad09aa66
Sahan Dhanujaya <sahandhanujaya18@gmail.com>
2026-08-11 03:57:51 +0530

    feat: new shop open

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://lakas-take-away.netlify.app"],
+    origin: ["http://localhost:5173", "https://katata-rasata.netlify.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
