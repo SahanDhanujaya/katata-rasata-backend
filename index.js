7a595498e3b062fe23a3f5aa00a8c193baa0e5b0
Sahan Dhanujaya <sahandhanujaya18@gmail.com>
2026-08-10 20:11:01 +0530

    update: current date

--- index.js
+++ index.js
@@ -226,7 +226,11 @@ app.get("/api/print/bill/:saleId", async (req, res) => {
       { type: 0, content: "0763243716", bold: 0, align: 1, format: 4 },
       {
         type: 0,
-        content: new Date(sale.date).toLocaleString(),
+        content: new Date().toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          hour12: true,
+        }),
         bold: 0,
         align: 1,
         format: 4,
